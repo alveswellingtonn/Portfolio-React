ecebd2a94ff544dfad78d8e06213b2c22fe218ca
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2022-09-28 18:10:29 -0300

    Desenvolvimento do Header

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <header>
+            
+            <nav>
+
+                <ul>
+                    <li>
+                        <a href='https://www.linkedin.com/in/wellington-alves-de-moraes/' target='_blank' rel='noreferrer'>Home</a>
+                    </li>
+                    <li>
+                        <a href='https://www.linkedin.com/in/wellington-alves-de-moraes/' target='_blank' rel='noreferrer'>Portfolio</a>
+                    </li>
+                    <li>
+                        <a href='https://www.linkedin.com/in/wellington-alves-de-moraes/' target='_blank' rel='noreferrer'>Contato</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </header>
+    );
+}
+
+export default Navbar;
